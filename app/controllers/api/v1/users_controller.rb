c1116246ed45ef9ebbd328cfe1cb55ffc3e24df6
ShannonPaige <spaige@gmail.com>
2017-07-14 12:00:32 -0600

    add index endpoint for user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  def index
+    respond_with User.all
+  end
+
   def create
     user = User.new(user_params)
     if user.save
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -3,6 +3,19 @@ require 'pry'
 
 class Api::V1::UsersControllerTest < ActionController::TestCase
 
+  test "#index responds with all users" do
+    get :index, format: :json
+    res = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal "Edgar", res[0]["first_name"]
+    assert_equal "Duran", res[0]["last_name"]
+    assert_equal "ed@email.com", res[0]["email"]
+    assert_equal "Mark", res[1]["first_name"]
+    assert_equal "Miranda", res[1]["last_name"]
+    assert_equal "mm@email.com", res[1]["email"]
+  end
+
   test "#create responds with created user when params are valid" do
     params = {
       first_name: "shannon",
